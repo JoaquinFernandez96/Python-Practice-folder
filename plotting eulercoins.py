bc2fa240e687a4e471922a51c576979412259aee
mmatroska <59630113+mmatroska@users.noreply.github.com>
2021-04-06 18:54:00 -0300

    test plotting, eulercoins

--- plotting eulercoins.py
+++ plotting eulercoins.py
@@ -6,7 +6,6 @@ tot=e
 min=e
 max=e
 
-temp=[]
 v_temp=[]
 min_temp=[]
 max_temp=[]
@@ -15,10 +14,10 @@ while True:
     if min==1:
         break
     v=min+max
+    print(v,min,max)
     v_temp.append(v)
     min_temp.append(min)
     max_temp.append(max)
-    temp+=1
     v%=mod
     if v>max:
         max=v
@@ -26,3 +25,12 @@ while True:
         min=v
         tot+=min    
 print("TOTAL:",tot)
+
+fig, ax = plt.subplots()  # Create a figure and an axes.
+# ax.plot(v_temp, label='V')  # Plot some data on the axes.
+ax.plot( min_temp , label='min')  # Plot more data on the axes...
+# ax.plot(max_temp, label='max')  # ... and some more.
+ax.set_xlabel('x label')  # Add an x-label to the axes.
+ax.set_ylabel('y label')  # Add a y-label to the axes.
+ax.set_title("eulercoin")  # Add a title to the axes.
+ax.legend()  # Add a legend.
