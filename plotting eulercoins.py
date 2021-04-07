4dfad8f13a5d60cb3fd8ca8bb7c1508f7e0fe160
mmatroska <59630113+mmatroska@users.noreply.github.com>
2021-04-07 00:50:27 -0300

    p euler 700

--- plotting eulercoins.py
+++ plotting eulercoins.py
@@ -1,36 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
-e=1504170715041707
-mod=4503599627370517
-tot=e
-min=e
-max=e
 
-v_temp=[]
-min_temp=[]
-max_temp=[]
+e = 1504170715041707
+mod = 4503599627370517
+tot = e
+min = e
+max = e
 
+v_temp = []
+min_temp = []
+max_temp = []
 while True:
-    if min==1:
+    if min == 1:
         break
-    v=min+max
-    print(v,min,max)
+    v = min + max
+    print(v, min, max)
     v_temp.append(v)
     min_temp.append(min)
     max_temp.append(max)
-    v%=mod
-    if v>max:
-        max=v
-    if v<min:
-        min=v
-        tot+=min    
-print("TOTAL:",tot)
+    v %= mod
+    if v > max:
+        max = v
+    if v < min:
+        min = v
+        tot += min
+print("TOTAL:", tot)
 
 fig, ax = plt.subplots()  # Create a figure and an axes.
-# ax.plot(v_temp, label='V')  # Plot some data on the axes.
-ax.plot( min_temp , label='min')  # Plot more data on the axes...
-# ax.plot(max_temp, label='max')  # ... and some more.
-ax.set_xlabel('x label')  # Add an x-label to the axes.
-ax.set_ylabel('y label')  # Add a y-label to the axes.
+ax.plot(v_temp[5:15], label="V")  # Plot some data on the axes.
+ax.plot(min_temp[5:15], label="min")  # Plot more data on the axes...
+ax.plot(max_temp[5:15], label="max")  # ... and some more.
+ax.set_xlabel("x label")  # Add an x-label to the axes.
+ax.set_ylabel("y label")  # Add a y-label to the axes.
 ax.set_title("eulercoin")  # Add a title to the axes.
 ax.legend()  # Add a legend.
